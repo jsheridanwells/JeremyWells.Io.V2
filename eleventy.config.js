d186f5ea7a07b739fa5290b18ebe811d1df708dd
jsheridanwells <jsheridanwells@gmail.com>
2023-04-17 08:36:38 -0500

    sass set up and configged

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,10 @@ const { DateTime } = require('luxon');
 
 module.exports = function(eleventyConfig) {
 	
+    eleventyConfig.setServerOptions({
+		watch: ['./_site/assets/css/**/*.css']
+	});
+
 	eleventyConfig.addFilter('asPostDate', date => {
 		return DateTime.fromJSDate(date).toLocaleString(DateTime.DATE_MED);
 	});
